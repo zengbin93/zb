5cd08172c58d1b608ddd81ca6c27a4fa9a746418
zengbin93 <zeng_bin8888@163.com>
2018-08-09 00:09:34 +0800

    add ZbList

--- zb/__init__.py
+++ zb/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __author__ = "zengbin93"
 
 
@@ -8,5 +8,5 @@ from zb.utils import elapsed, create_logger
 import zb.functions as functions
 import zb.nlp as nlp
 import zb.tools as tools
-
+from zb.base import ZbDict, ZbList
 
--- zb/base.py
+++ zb/base.py
@@ -7,3 +7,22 @@ class ZbDict(dict):
     def __init__(self, *args, **kwargs):
         super(ZbDict, self).__init__(*args, **kwargs)
         self.__dict__ = self
+
+
+class ZbList(list):
+    def __init__(self, *args, **kwargs):
+        super(ZbList, self).__init__(*args, **kwargs)
+
+    def discrete_index(self, indices):
+        """get elements by discrete indices
+
+        :param indices: list
+            discrete indices
+        :return: elements
+        """
+        elements = []
+        for i in indices:
+            elements.append(self[i])
+        return elements
+
+
