5bad2b4c1edf0402555f732160a096fc8a8226e4
zengbin93 <zeng_bin8888@163.com>
2018-10-09 13:21:15 +0800

    add merge_pdf

--- zb/tools/img.py
+++ zb/tools/img.py
@@ -1,10 +1,17 @@
+# -*- coding: utf-8 -*-
 
 from PIL import Image
 import os
 
 
 def img2pdf(path, pdf_name=None):
-    """merge images in path into a pdf file"""
+    """合并path目录下的所有图片到pdf文件中
+
+    :param path: str
+        图片路径
+    :param pdf_name: str
+        默认值 图片路径.pdf; pdf文件名
+    """
     print("merge images in %s ..." % path)
     if not pdf_name:
         pdf_name = os.path.join(path, '%s.pdf' % os.path.split(path)[1])
@@ -33,15 +40,6 @@ def img2pdf(path, pdf_name=None):
             im_list.append(img)
         else:
             im_list.append(img)
-    im1.save(pdf_name, "PDF", resolution=100.0, save_all=True, append_images=im_list)
+    im1.save(pdf_name, "PDF", resolution=100.0, save_all=True,
+             append_images=im_list)
     print("images saved to：", pdf_name)
-
-
-if __name__ == "__main__":
-    from concurrent.futures import ProcessPoolExecutor
-    fail = []
-    word_dir = r'E:\8 试制设备费'
-    all_path = [os.path.join(word_dir, path)
-                for path in os.listdir(word_dir)]
-    with ProcessPoolExecutor(2) as executor:
-        executor.map(img2pdf, all_path)
--- zb/tools/pdf.py
+++ zb/tools/pdf.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import os
+import PyPDF2
 from urllib.request import urlopen, Request
 from pdfminer.pdfparser import PDFParser, PDFDocument
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
@@ -56,3 +58,24 @@ def pdf2text(path, password="", headers=None):
     return text
 
 
+def merge_pdf(pdf_files, output_file):
+    """合并多个pdf文件
+
+    :param pdf_files: list
+        需要合并的pdf文件列表
+    :param output_file: str
+        合并后的pdf文件路径
+    """
+    merger = PyPDF2.PdfFileMerger()
+    for file in pdf_files:
+        f = open(file, 'rb')
+        file_rd = PyPDF2.PdfFileReader(f)
+        # use file name as bookmark
+        bookmark = os.path.basename(os.path.splitext(file)[0])
+        if file_rd.isEncrypted:
+            print('%s is encrypted, not supported!' % file)
+            continue
+        merger.append(file_rd, bookmark=bookmark, import_bookmarks=True)
+        f.close()
+    merger.write(output_file)
+    merger.close()
