93113b174c58665d1d7e79f17efb80344c8cb172
zengbin93 <zeng_bin8888@163.com>
2018-10-26 15:51:29 +0800

    modify

--- zb/crawlers/xinshipu.py
+++ zb/crawlers/xinshipu.py
@@ -9,6 +9,8 @@ https://www.xinshipu.com/
 import requests
 from bs4 import BeautifulSoup
 import re
+import time
+from urllib.parse import urljoin
 
 from zb.crawlers.utils import get_header
 
@@ -19,35 +21,61 @@ def get_recipe_detail(recipe_url):
     """从url中获取菜谱详细信息
 
     :param recipe_url: str
-        菜谱url，如：https://www.xinshipu.com/zuofa/598775
+        菜谱url，如：https://www.xinshipu.com/zuofa/598775;
+        https://www.xinshipu.com//zuofa/749342
     :return:dict
     """
-    # recipe_url = "https://www.xinshipu.com/zuofa/598775"
     response = requests.get(recipe_url, headers=get_header())
     html = BeautifulSoup(response.text, 'lxml')
 
     # 获取菜名
     name = html.find("div", {"class": "re-up"}).h1.text
 
+    # 主图
+    img = html.find("div", {"class": "gallery"}).a['href']
+    img = urljoin(HOME_URL, img)
+
     all_info = html.find_all("div", {"class": "dd"})
 
-    # 简介
-    intro = re.sub('\n|\t|\r| ', '', all_info[0].text)
+    if len(all_info) == 4:
+        # 简介
+        intro = re.sub('\n|\t|\r| ', '', all_info[0].text)
+        material_i = 1
+        method_i = 2
+    else:
+        intro = None
+        material_i = 0
+        method_i = 1
 
     # 食材
-    material = all_info[1].text.strip()
-    material = re.sub('\r\n|\r\n \n', '\n', material)
+    material = all_info[material_i].text.strip()
+    material = re.sub('\r\n|\r\n \n|\n\n\n', '\n', material)
 
     # 做法
-    method = all_info[2].text.strip()
-    method = re.sub('\r\n|\r\n \n', '\n', method)
+    try:
+        method_steps = html.find("ol", {"class": "re-step-wpic"}).find_all('li')
+        method = []
+        for i, m in enumerate(method_steps, 1):
+            step = dict(step_num=i)
+            step['text'] = m.text.strip()
+            if m.img:
+                step['img_url'] = urljoin(HOME_URL, m.img['src'])
+            method.append(step)
+    except:
+        method = all_info[method_i].text.strip()
+        method = re.sub('\r\n|\r\n \n|\n\n\n\n', '\n', method)
 
     # 相关菜品
-    classify = all_info[4].text.strip().replace('\xa0\xa0', ' | ')
+    classify = all_info[-1].text.strip()
+    if '\xa0\xa0' in classify:
+        classify = classify.replace('\xa0\xa0', ' | ')
+    else:
+        classify = ""
 
     return {
         "name": name,
         "url": recipe_url,
+        "img": img,
         "intro": intro,
         "material": material,
         "method": method,
@@ -66,19 +94,20 @@ def get_all_classify():
 
     for a in all_a:
         if a.has_attr('rel') and not a.has_attr('class'):
-            class_url = HOME_URL + a['href']
+            class_url = urljoin(HOME_URL, a['href'])
             classify[a.text] = class_url
 
     return classify
 
 
-def get_class_recipes(class_url):
+def get_class_recipes(class_url, max_page=20, sleep=0.1):
     """获取某个菜谱分类url下的所有菜谱url"""
     class_url = class_url + "?page={page}"
     recipes = dict()
 
     # 暴力爬取方案，每个菜谱分类请求100页
-    for page in range(1, 100):
+    for page in range(1, max_page):
+        time.sleep(sleep)
         url = class_url.format(page=page)
         print("current url: ", url)
         response = requests.get(url, headers=get_header())
@@ -87,10 +116,11 @@ def get_class_recipes(class_url):
         # 获取本页菜谱
         menus = html.find("div", {'class': 'new-menu-list search-menu-list clearfix mt10'})
         if menus:
+            print("get recipes fail: ", url)
             menus = menus.find_all('a')
             for m in menus:
                 name = re.sub("\n| ", "", m.text)
-                recipe_url = HOME_URL + m['href']
+                recipe_url = urljoin(HOME_URL, m['href'])
                 recipes[name] = recipe_url
 
         # 判断是否是最后一页
@@ -105,9 +135,12 @@ def get_class_recipes(class_url):
 if __name__ == '__main__':
     import json
     import os
+    import time
+
     data_path = r"C:\ZB\git_repo\recipes"
 
     # 分类url
+    print("获取分类url ...")
     file_classify = os.path.join(data_path, "classify_url.json")
     if os.path.exists(file_classify):
         classify = json.load(open(file_classify, 'r', encoding='utf-8'))
@@ -117,29 +150,42 @@ if __name__ == '__main__':
                   indent=2, ensure_ascii=False)
 
     # 食谱url
+    print("获取食谱url ...")
     file_recipes = os.path.join(data_path, "recipes_url.json")
     if os.path.exists(file_recipes):
         recipes_url = json.load(open(file_recipes, 'r', encoding='utf-8'))
     else:
         recipes_url = dict()
         for class_url in classify.values():
-            class_recipes = get_class_recipes(class_url)
-            recipes_url.update(class_recipes)
-        json.dump(recipes_url, open(file_recipes, 'w', encoding='utf-8'),
-                  indent=2, ensure_ascii=False)
+            try:
+                class_recipes = get_class_recipes(class_url)
+                recipes_url.update(class_recipes)
+                print("current total recipes number is ", len(recipes_url))
+                json.dump(recipes_url, open(file_recipes, 'w', encoding='utf-8'),
+                          indent=2, ensure_ascii=False)
+            except:
+                continue
 
     # 食谱详细信息
+    print("获取食谱详细信息 ...")
     file_recipes_detail = os.path.join(data_path, "recipes_detail.json")
     if os.path.exists(file_recipes_detail):
-        recipes_detail = json.load(open(file_recipes_detail, 'r'))
+        recipes_detail = json.load(open(file_recipes_detail, 'r', encoding='utf-8'))
     else:
         recipes_detail = dict()
-    for recipe_url in recipes_url:
+    for recipe_url in recipes_url.values():
+        time.sleep(0.1)
         if recipe_url in recipes_detail.keys():
             continue
-        print("current: ", recipe_url)
-        detail = get_recipe_detail(recipe_url)
-        recipes_detail[recipe_url] = detail
+        try:
+            print("current: ", recipe_url)
+            detail = get_recipe_detail(recipe_url)
+            recipes_detail[recipe_url] = detail
+            json.dump(recipes_detail, open(file_recipes_detail, 'w', encoding='utf-8'),
+                      indent=2, ensure_ascii=False)
+        except:
+            print("fail: ", recipe_url)
+            continue
 
 
 
